54e898d2d54bad0e70ad7eb49cedc546535f6b30
Adhasetiawan <adhasetiawanwiyana@gmail.com>
2022-10-24 20:23:15 +0800

    track record page has been changed with new UI style

--- lib/features/track_record/page_trackrecord.dart
+++ lib/features/track_record/page_trackrecord.dart
@@ -5,7 +5,6 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:tester_app/features/track_record/controller_trackrecord.dart';
-import 'package:timeline_tile/timeline_tile.dart';
 
 class PageTrackrecord extends GetView<ControllerTrackrecord> {
   const PageTrackrecord({Key? key}) : super(key: key);
@@ -13,48 +12,51 @@ class PageTrackrecord extends GetView<ControllerTrackrecord> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: const Color(0xff5C94E8),
-        title: const Text(
-          'Track Record',
-          style: TextStyle(
-              fontSize: 30, color: Colors.white, fontFamily: 'ComicSans'),
-        ),
-      ),
-      body: Obx(() => RefreshIndicator(
-        onRefresh: () async {
-          await Future.delayed(const Duration(seconds: 3));
-        },
-        child: Column(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.center,
-              height: 120,
-              decoration: const BoxDecoration(
-                  color: Color(0xff5C94E8),
-                  borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(20),
-                    bottomLeft: Radius.circular(20),
-                  )),
-              child: Column(
-                children: [
+      backgroundColor: Colors.grey.withOpacity(0.1),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(150),
+        child: AppBar(
+          centerTitle: true,
+          elevation: 4,
+          backgroundColor: Colors.white,
+          flexibleSpace: Container(
+            padding: const EdgeInsets.fromLTRB(16, 40, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Track Record',
+                    style: GoogleFonts.nunito(
+                        textStyle: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold
+                        )
+                    ),
+                  ),
+                  Text(
+                    'Atur tanggal spesifik',
+                    style: GoogleFonts.nunito(
+                        textStyle: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.black
+                        )
+                    ),
+                  ),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
                       GestureDetector(
                         onTap: () => controller.selectedDateOne(context),
                         child: Container(
                           height: 40,
-                          width: 95,
+                          width: Get.width * 0.4,
                           margin: const EdgeInsets.only(top: 20),
-                          decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(10),
-                                topLeft: Radius.circular(10),
-                              )),
+                          decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(color: Colors.black54, width: 1),
+                              borderRadius: BorderRadius.circular(10)
+                          ),
                           alignment: Alignment.center,
                           child: Text(
                               DateFormat('yyyy-MM-dd').format(controller.date_one.value)
@@ -65,9 +67,13 @@ class PageTrackrecord extends GetView<ControllerTrackrecord> {
                         onTap: ()=> controller.selectedDateTwo(context),
                         child: Container(
                           margin: const EdgeInsets.only(left: 5, right: 5, top: 20),
-                          color: Colors.white,
+                          decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(color: Colors.black54, width: 1),
+                              borderRadius: BorderRadius.circular(10)
+                          ),
                           height: 40,
-                          width: 95,
+                          width: Get.width * 0.4,
                           alignment: Alignment.center,
                           child: Text(
                               DateFormat('yyyy-MM-dd').format(controller.date_two.value)
@@ -80,95 +86,165 @@ class PageTrackrecord extends GetView<ControllerTrackrecord> {
                           // Get.toNamed(Routes.detail_track_record);
                         },
                         child: Container(
-                          margin: const EdgeInsets.only(top: 20),
-                          height: 40,
-                          width: 55,
-                          decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                bottomRight: Radius.circular(10),
-                                topRight: Radius.circular(10),
-                              )),
+                          margin: const EdgeInsets.only(top: 20, left: 3),
+                          decoration: BoxDecoration(
+                              color: const Color(0xff5C94E8),
+                              borderRadius: BorderRadius.circular(10)
+                          ),
                           alignment: Alignment.center,
                           child: const Icon(
-                            CupertinoIcons.search,
-                            color: Color(0xff5C94E8),
+                            Icons.filter_alt,
+                            color: Colors.white,
+                            size: 40,
                           ),
                         ),
                       )
                     ],
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const Text(
-                      'Pastikan tanggal sudah benar ya',
-                      style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white,
-                          fontFamily: 'ComicSans'),
-                    ),
-                  ),
-                ],
-              ),
+                ]
             ),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: controller.trackRecordList.length,
-                    padding: EdgeInsets.only(left: 30, right: 15),
-                    itemBuilder: (context, index)=>
-                        GestureDetector(
-                          onTap: ()=> Get.snackbar(
-                              'Mobile Presence',
-                              'This is just sample',
-                              snackPosition: SnackPosition.BOTTOM
-                          ),
-                          child: SizedBox(
-                            height: 140,
-                            child: TimelineTile(
-                              alignment: TimelineAlign.start,
-                              isFirst: index == 0 ? true : false,
-                              isLast: index == controller.trackRecordList.length -1 ? true : false,
-                              indicatorStyle: IndicatorStyle(
-                                  height: 12,
-                                  width: 12,
-                                  color: Color(0xff5C94E8)
-                              ),
-                              beforeLineStyle: LineStyle(
-                                  color: Color(0xff5C94E8),
-                                  thickness: 2
-                              ),
-                              endChild: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.fromLTRB(10, 55, 0, 0),
-                                    child: Text(
-                                      DateFormat('dd/MM/yyyy').format(controller.trackRecordList[index].date!),
-                                      style: GoogleFonts.comicNeue(
-                                          fontSize: index == 0 ? 20 : 16,
+          ),
+        ),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await Future.delayed(const Duration(seconds: 3));
+        },
+        child: Expanded(
+            child: ListView.builder(
+                itemCount: 5,
+                padding: const EdgeInsets.only(left: 30, right: 15),
+                itemBuilder: (context, index)=>
+                    GestureDetector(
+                      onTap: ()=> Get.snackbar(
+                          'Mobile Presence',
+                          'This is just sample',
+                          snackPosition: SnackPosition.BOTTOM
+                      ),
+                      child: Container(
+                        margin: EdgeInsets.only(bottom: index == 0 ? 20 : 0),
+                        padding: EdgeInsets.only(bottom: index == 0 ? 20 : 5),
+                        decoration: index == 0 ? const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              width: 2,
+                              color: Colors.black38
+                            )
+                          )
+                        ) : null,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Visibility(
+                              visible: index == 0 ? true : false,
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: 16),
+                                child: Text(
+                                  'Hari ini',
+                                  style: GoogleFonts.nunito(
+                                      textStyle: const TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.black,
                                           fontWeight: FontWeight.bold
-                                      ),
-                                    ),
+                                      )
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.fromLTRB(10, 5, 60, 0),
-                                    child: Text(
-                                      controller.trackRecordList[index].location ?? 'Unknown',
-                                      style: GoogleFonts.comicNeue(
-                                          fontSize: 16
+                                ),
+                              ),
+                            ),
+                            Card(
+                              elevation: 4,
+                              color: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18),
+                              ),
+                              child: Row(
+                                children: <Widget>[
+                                  Column(
+                                    children: <Widget>[
+                                      Container(
+                                        margin: const EdgeInsets.only(left: 16, top: 16),
+                                        width: 70,
+                                        height: 20,
+                                        alignment: Alignment.center,
+                                        decoration: const BoxDecoration(
+                                          color: Color(0xff6496E6),
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(10),
+                                            topRight: Radius.circular(10),
+                                          )
+                                        ),
+                                        child: Text(
+                                            'Oct 2022',
+                                          style: GoogleFonts.nunito(
+                                            color: Colors.white,
+                                            fontSize: 10
+                                          ),
+                                        ),
                                       ),
-                                    ),
+                                      Container(
+                                        margin: const EdgeInsets.only(left: 16, bottom: 16),
+                                        width: 70,
+                                        height: 45,
+                                        alignment: Alignment.center,
+                                        decoration: BoxDecoration(
+                                          color: Colors.transparent,
+                                          borderRadius: const BorderRadius.only(
+                                            bottomLeft: Radius.circular(10),
+                                            bottomRight: Radius.circular(10),
+                                          ),
+                                          border: Border.all(width: 2, color: const Color(0xff6496E6))
+                                        ),
+                                        child: Text(
+                                            '01',
+                                          style: GoogleFonts.nunito(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(width: 15),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                          'Check in pada 09:00',
+                                        style: GoogleFonts.nunito(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                          fontSize: 18
+                                        ),
+                                      ),
+                                      Row(
+                                        children: <Widget>[
+                                          const Icon(
+                                            Icons.location_on,
+                                            size: 20,
+                                            color: Color(0xff6496E6),
+                                          ),
+                                          Text(
+                                            'On-Site',
+                                            style: GoogleFonts.nunito(
+                                                color: Colors.black,
+                                                fontSize: 14
+                                            ),
+                                          ),
+                                        ],
+                                      )
+                                    ],
                                   ),
                                 ],
                               ),
-                            ),
-                          ),
-                        )
-                )
-            ),
-          ],
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+            )
         ),
-      )),
+      ),
     );
   }
 }
